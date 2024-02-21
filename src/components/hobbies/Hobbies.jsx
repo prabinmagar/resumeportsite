7e5245fd0a57c31486819541a03b749dd2453063
prabin.thapamagar <prabin_mi@yonefu.info>
2024-02-21 15:19:09 +0545

    kadsf

--- src/components/hobbies/Hobbies.jsx
+++ src/components/hobbies/Hobbies.jsx
@@ -13,7 +13,7 @@ const Hobbies = () => {
               <HobbiesItem key={item.id} item={item} />
             ))}
           </div>
-          <div className="dots-shape"></div>
+          <div className="dots-shape" style={{ marginLeft: "-9px"}}></div>
         </div>
       </div>
     </div>
